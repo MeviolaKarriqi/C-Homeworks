c30c2d48dce8234997ba934c638eb87c7656e97e
MeviolaKarriqi <82405299+MeviolaKarriqi@users.noreply.github.com>
2021-04-12 21:03:23 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,104 +1,70 @@
 /*
- * Ndertoni nje klase qe per disqet e nje videoteke , ruan daten e daljes ne treg dhe kohezgjatjen e diskut.
- * Klasa duhet te permbaje nje konstruktor me parametra, nje metode qe do te vendose vlerat e variablave
- * te instances dhe dy metoda qe do te kthejne vlerat e tyre.
- * Ndertoni nje klase per albumet e kengeve e cila do te derivoje nga klasa e disqeve.
- * Kjo klase do te ruaje titullin e albumit, si dhe nje vektor me rekorde, qe do te mbaje titujt dhe kengetaret
- * e kengeve te albumit. Klasa ne konstruktorin e saj, nder te tjera, do ti jape vlere titullit dhe do te populloje
- * vektorin duke i marre vlerat nga nje skedar. Ky skedar, ne cdo dy rreshta te tij do te mbaje te dhenat e nje kenge.
- * Ai do te kete pamjen e meposhtme:
- *
- * Kenge.txt
- * kengetar1
- * titull1
- * kengetar2
- * titull2
+ * 1.Ndertoni nje klase Karte_Klienti per bleresit e nje rrjeti supermarketesh. Per nje kate do te ruhet emri I
+ * zoteruesit te kartes, numri aktuel I pikeve dhe nje container me blerjet e kryera. Per cdo blerje te ruhet vlera ne
+ * leke e blerjes dhe piket qe perfitohen. (krijoni nje tip te ri te strukturuar per blerjet)
+ * Klasa duhet te ofroje keto veprime:
+ * Te jape mundesine e krijimit te nje karte klienti duke specifikuar emrin, piket dhe emrin e nje skedari qe do te permbaje
+ * te dhenat e blerjeve te kryera. Ne kete skedar cdo blerje do te jete ne rresht te vecante dhe vlerat e blerjes te ndara me hapesira.
+ * Piket e nje karte do te llogariten duke marre parasysh gjithe blerjet e kryera. Merrni parasysh perdorimin e kujteses ne rastet kur
+ * skedari mund te kete nje nr shume te madh blerjesh. (30 pike)
+ * Te kryeje blerje me nje vlere te caktuar dhe te modifikoje te dhenat e kartes pas blerjes. Per cdo 10 leke perfitohet 1 pike. (10pike)
+ * Te ktheje zoteruesin, numrin e pikeve si dhe vleren totale te blerjeve. (10 pike)
+ * Perdorni klasen dhe gjithe funksionalitet e saj. (10 pike)
  */
-
 #include <iostream>
-#include <vector>
 #include <fstream>
+
 using namespace std;
 
-class Disqe{
+class Karte_Klienti{
 private:
-    string data;
-    string kohezgjatja;
-public:
-    Disqe(string date , string kohe){
-        data = date;
-        kohezgjatja = kohe;
-    }
-    string getDaten (){
-        return data;
-    }
-    void setDaten(string dataa){
-        this->data = dataa;
-    }
-    string getKhezgjatjen (){
-        return kohezgjatja;
-    }
-    void setKohezgjatjen(string kohee){
-        this->kohezgjatja = kohee;
-    }
-};
-class Albume : public Disqe{
-private:
-    string emriIAlbumit;
-    vector<string>tituj;
-public:
-    Albume(string date, string kohe, string emri, vector<string> titujj) : Disqe(date, kohe) {
-        emriIAlbumit = emri;
-        tituj =titujj;
-    }
+    string emriIKlientit;
+    double numriAktualIPikeve;
 
-    string getEmri(){
-        return emriIAlbumit;
-    }
-    void setEmri(string emri){
-        emriIAlbumit = emri;
-    }
-    vector<string> getTituj(){
-        return tituj;
-    }
-    void setTituj(vector<string>titujj){
-        tituj = titujj;
-    }
-    bool mbushVecMeAneTeSkedarit(string emriSkedarit , vector<string> & vecITitujve){
-        ifstream in(emriSkedarit.c_str());
-        if(!in){
-            cerr<<"Nuk mundemi te hapim kete skedar! "<<endl;
-            return false;
-        }
-        string str;
-        while(getline(in, str)){
-            if (str.size()>0){
-                vecITitujve.push_back(str);
-            }
-        }
-        in.close();
-        return true;
-    }
 };
 
 int main() {
-    string data;
-    string koha;
-    string emriIAlbumit;
-    vector<string>tituj;
-    cout<<"Jepni daten e daljes ne treg te albumit : "<<endl;
-    cin>>data;
-    cout<<"Jepni kohezjatjen e albumit : "<<endl;
-    cin>>koha;
-    cout<<"Jepni emrin e Albumit : "<<endl;
-    cin>>emriIAlbumit;
-    Albume albume(data, koha, emriIAlbumit, tituj);
-    bool titujtt = albume.mbushVecMeAneTeSkedarit("albume.txt", tituj);
-    if (titujtt){
-        for (string & line : tituj){
-            cout<<line<<endl;
-        }
-    }
+
 
     return 0;
+
 }
+/*
+ * fstream file; //object of fstream class
+
+    //opening file "sample.txt" in out(write) mode
+    file.open("sample.txt",ios::out);
+
+    if(!file)
+    {
+        cout<<"Error in creating file!!!"<<endl;
+        return 0;
+    }
+
+    cout<<"File created successfully."<<endl;
+    //write text into file
+    file<<"ABCD.";
+    //closing the file
+    file.close();
+
+    //again open file in read mode
+    file.open("sample.txt",ios::in);
+
+    if(!file)
+    {
+        cout<<"Error in opening file!!!"<<endl;
+        return 0;
+    }
+
+    //read untill end of file is not found.
+    char ch; //to read single character
+    cout<<"File content: ";
+
+    while(!file.eof())
+    {
+        file>>ch; //read single character from file
+        cout<<ch;
+    }
+
+    file.close(); //close file
+ */
